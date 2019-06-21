6ca90584944fa10b91b1cc0f1f411fd5a71ead20
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-21 14:32:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(path, english_emoticon)
       return jap
     end
   end
-  return "Sorry, that emoticon was not found"
+  "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
